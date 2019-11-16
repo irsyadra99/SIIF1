6e47545d586154a85643bc37057b201c4b2638cf
Irsyad <irsyad099@gmail.com>
2019-11-16 20:45:43 +0700

    cuma tambah method aja

---
+++ inventaris.php
@@ -0,0 +1,17 @@
+<?php
+class Inventaris{
+    var $nama_model,
+        $kode_barang,
+        $tanggal_masuk,
+        $asal_barang,
+        $foto,
+        $jumlah,
+        $lokasi,
+        $tipe_barang;
+        function __construct(){
+
+        }
+        function inventaris($nama, $kode, $tanggal, $asal, $foto, $jumlah, $lokasi, $tipe){
+            
+        }
+}
---
+++ pengurus.php
@@ -0,0 +1,6 @@
+<?php
+class pengurus{
+    function addBarang($nama, $kode, $tanggal, $asal, $jumlah, $lokasi, $tipe){
+
+    }
+}
