b7675ef091b18d703150641b93b9d20aac670a2a
Irsyad <irsyad099@gmail.com>
2019-11-19 08:33:16 +0700

    update2

--- akademisi.php
+++ akademisi.php
@@ -6,11 +6,18 @@
      function lapor($kodebrg, $deskripsi, $foto, $tipe, $lokasi){
          include "database.php";
          $mysqli = mysqli_connect("remotemysql.com", "kZXrSwhZy7", "SLjmGMemEO", "kZXrSwhZy7");
-         $result = $mysqli->query("INSERT INTO Laporan_Kerusakan ('idLaporanK', 'kode_barang', 'deskripsi', 'foto', 'tipe_barang', 'lokasi')VALUES('', '$kodebrg', '$deskripsi', '$foto', '$tipe', '$lokasi')");      
+         $result = $mysqli->query("INSERT INTO Laporan_Kerusakan VALUES('', '$kodebrg', '$deskripsi', '$foto', '$tipe', '$lokasi')");      
      }
  }
- $ak= new akademisi();
- $ak->lapor($_POST['kode_barang'], $_POST['deskripsi'], $_POST['bukti'], $_POST['tipe'], $_POST['lokasi']);
+ if(isset($_POST['lapor'])){
+    $kode_barang = $_POST['kode_barang'];
+    $deskripsi = $_POST['deskripsi'];
+    $foto = $_POST['bukti'];
+    $tipe = $_POST['tipe'];
+    $lokasi = $_POST['lokasi'];
+    $ak= new akademisi();
+    $ak->lapor($kode_barang, $deskripsi, $foto, $tipe, $lokasi);
+ }
 ?>
 <form method="POST" action="" name="lapor">
     Kode Barang :
